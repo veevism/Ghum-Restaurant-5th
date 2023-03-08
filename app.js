4715ce0b6fa346b75e5e6ddd15c267e9e96c0916
Phiriyakorn Maneekongrit <phiriyakornman@gmail.com>
2023-03-08 15:50:21 +0700

    Add new index

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public'))); 
+app.use('/js', express.static(__dirname + 'controller'));
 
 app.get('/', (req, res) => {
     res.render('index');
---
+++ public/js/nav.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+});
