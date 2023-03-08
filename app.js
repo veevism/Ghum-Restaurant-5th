93e968963dab46cf8a7670459fc294696059065c
sorawee <veevisorawee@gmail.com>
2023-03-08 17:00:32 +0700

    Relocate some lines

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require('path');
+const path = require("path");
 // const ejs = require("ejs");
 // const _ = require('lodash');
 
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public'))); 
-app.use('/js', express.static(__dirname + 'controller'));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/js", express.static(__dirname + "controller"));
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 app.listen(port, function () {
-    console.log(`Server app listening on port ${port}`)
+  console.log(`Server app listening on port ${port}`);
+  console.log("http://localhost:3000/");
 });
