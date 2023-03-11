8362ebd8512323391359de9432fd4e845b09fc90
sorawee <veevisorawee@gmail.com>
2023-03-11 16:02:16 +0700

    add db config

--- app.js
+++ app.js
@@ -4,26 +4,20 @@ const path = require("path");
 
 const menus = require("./public/js/menus");
 const { json } = require("body-parser");
+const db = require("./config/db");
 // const ejs = require("ejs");
 // const _ = require('lodash');
-const mongoose = require("mongoose");
 require("dotenv").config();
+
+db.connect();
 
 //for connct atlas
 // const dbURL = process.env.MONGODB_URL;
 
 //for connect local
-const dbURL =
-  "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.0";
 
 const app = express();
 const port = 3000;
-
-mongoose
-  .connect(dbURL)
-  .then(() => console.log("Connected!"))
-  .catch(() => console.log("Something went wrong with DB"));
-
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -50,15 +44,13 @@ app.get("/admin", (req, res) => {
 
 //404 handling
 app.use((req, res, next) => {
-  res.status(404).catch(res.redirect("/"))
+  res.status(404).catch(res.redirect("/"));
 });
 
 // console.log(new Error('A standard error'))
 
 // console.log("mmm",menus.menu[3])
 
-
 app.listen(process.env.PORT, function () {
-  console.log(`Server app listening on port ${port}`);
-
+  console.log(`Server app listening on port ${process.env.PORT}`);
 });
---
+++ config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const dbURL = process.env.MONGODB_URL;
+const dbLocalURL = "mongodb://127.0.0.1:27017/GhumDB";
+
+exports.connect = () => {
+  mongoose
+    .connect(dbLocalURL)
+    .then(() => console.log("Connected!"))
+    .catch(() => console.log("Something went wrong with DB"));
+};
