a010e044236f5d61e8385cef0a0940d47cb06fd7
sorawee <veevisorawee@gmail.com>
2023-03-17 19:27:00 +0700

    create schema and add to mongo

--- app.js
+++ app.js
@@ -5,29 +5,44 @@ const path = require("path");
 const menus = require("./public/js/menus");
 const { json } = require("body-parser");
 const db = require("./config/db");
+
+const { Menu } = require("./model/schema");
 // const ejs = require("ejs");
 // const _ = require('lodash');
+
 require("dotenv").config();
-
 db.connect();
-
 //for connct atlas
 // const dbURL = process.env.MONGODB_URL;
 
 //for connect local
 
+const special1 = new Menu({
+  title: "Hello",
+  price: 64.2,
+});
+
+// Menu.collection.insertOne(special1).then(() => console.log("add 1 items"));
 const app = express();
 const port = 3000;
 
+// console.log(Menus.find(special1));
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/js", express.static(__dirname + "controller"));
+// app.use(express.static(path.join(__dirname, "public")));
+// app.use("/js", express.static(__dirname + "controller"));
 
-app.get("/", (req, res) => {
+//vvadded
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", async (req, res) => {
   res.render("index", {
     menus: menus,
   });
+
+  const menuss = Menu.find({});
+  console.log(menuss.length);
 });
 
 app.get("/login", (req, res) => {
@@ -55,9 +70,9 @@ app.get("/status", (req, res) => {
 });
 
 //404 handling
-app.use((req, res, next) => {
-  res.status(404).catch(res.redirect("/"));
-});
+// app.use((req, res, next) => {
+//   res.status(404).catch(res.redirect("/"));
+// });
 
 // console.log(new Error('A standard error'))
 
---
+++ model/schema.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+// const db = require("../config/db");
+
+const menuSchema = new mongoose.Schema({
+  id: Number,
+  title: String,
+  category: String,
+  price: mongoose.Types.Decimal128,
+  img: String,
+  desc: String,
+  quantity: Number,
+});
+
+exports.Menu = mongoose.model("menu", menuSchema);
